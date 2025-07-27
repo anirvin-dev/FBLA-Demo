71197b9c84f3a259455336c4bbb18655a5fd6d00
Ankit-Panda-2010 <ankitpanda2010@gmail.com>
2025-07-27 18:12:35 -0400

    Added Variables to hold service instances and dependencies

--- apps/basecamp/src/data/attendance/attendance.service.spec.ts
+++ apps/basecamp/src/data/attendance/attendance.service.spec.ts
@@ -5,4 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 describe('AttendanceService', () => {
+    let service: AttendanceService;
+    let sheetService: jest.Mocked<SheetService>;
+    let configService: jest.Mocked<ConfigService>;
   });
