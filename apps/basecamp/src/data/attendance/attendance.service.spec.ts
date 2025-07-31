f28f7ca8662b094c940ec24258283d79aa83d026
Ankit-Panda-2010 <ankitpanda2010@gmail.com>
2025-07-31 19:43:15 -0400

    Imported logger

--- apps/basecamp/src/data/attendance/attendance.service.spec.ts
+++ apps/basecamp/src/data/attendance/attendance.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AttendanceService } from './attendance.service';
 import { SheetService } from '../../sheet/sheet.service';
 import { ConfigService } from '@nestjs/config';
+import {Logger} from '@nestjs/common';
 
 describe('AttendanceService', () => {
     let service: AttendanceService;
