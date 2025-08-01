efc75f2e898b5e18bdd2ed64713fab7f2be42971
Ankit-Panda-2010 <ankitpanda2010@gmail.com>
2025-07-31 20:56:48 -0400

    Should fix build errors?

--- apps/basecamp/src/data/attendance/attendance.service.spec.ts
+++ apps/basecamp/src/data/attendance/attendance.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AttendanceService } from './attendance.service';
 import { SheetService } from '../../sheet/sheet.service';
 import { ConfigService } from '@nestjs/config';
-import {Logger} from '@nestjs/common';
+
 
 describe('AttendanceService', () => {
     let service: AttendanceService;
@@ -132,7 +132,7 @@ describe('AttendanceService', () => {
                         'user2': 3,
                         'user3': 5
                     };
-                    return hoursMap[discordId] || 0;
+                    return Promise.resolve(hoursMap[discordId] || 0);
                 });
         });
 
