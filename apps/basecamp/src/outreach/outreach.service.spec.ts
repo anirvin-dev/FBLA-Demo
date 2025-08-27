f2740cfa61901ed15dedefb9e0bfc0b042f16047
Drew Beamer <andrewmbeamer@gmail.com>
2025-08-26 23:25:43 -0400

    update module name mapping in jest tests

--- apps/basecamp/src/outreach/outreach.service.spec.ts
+++ apps/basecamp/src/outreach/outreach.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OutreachService } from './outreach.service';
-import { SheetService } from '../../sheet/sheet.service';
+import { SheetService } from '../sheet/sheet.service';
 import { ConfigService } from '@nestjs/config';
 
 describe('OutreachService', () => {
